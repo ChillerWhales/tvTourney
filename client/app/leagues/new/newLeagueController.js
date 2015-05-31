fa1dcd88699e222f804f04c90c8e9df71761254f
Monica Engel <monicaengel9@gmail.com>
2015-05-31 13:43:52 -0700

    [feature] WIP create new league controller post request

--- client/app/leagues/new/newLeagueController.js
+++ client/app/leagues/new/newLeagueController.js
@@ -1,6 +1,39 @@
 angular.module('app.leagues.new', [])
-.controller('createLeagueCtrl', function ($scope) {
-  $scope.leagueName;
+.factory('postNewLeague', function($http){
+  var league = {};
+  return {
+    saveLeagueAttr: function (leagueName, tvShow, weekDay, showTime, rosterLimit) {
+      league.name = leagueName;
+      league.show = tvShow;
+      league.roster_limit = rosterLimit;
+      league.week_day = weekDay;
+      league.show_time = showTime;
+    },
+    postLeague: function() {
+      $http({
+        method: 'POST',
+        url: '/league/',
+        data: league,
+      })
+      .success(function (res) {
+        callback(true, res);
+      })
+      .error(function (err) {
+        callback(false, err);
+      });
+    }
+  }
+})
+.controller('createLeagueCtrl', function ($scope, allLeagueAttr) {
+  $scope.newLeague = {};
+  $scope.saveLeagueInfo(leagueName, tvShow, weekDay, showTime, rosterLimit) {
+    allLeagueAttr.saveLeagueAttr(leagueName, tvShow, weekDay, showTime, rosterLimit);
+    // $scope.newLeague.name = leagueName;
+    // $scope.newLeague.show = tvShow;
+    // $scope.newLeague.weekday = weekDay;
+    // $scope.newLeague.showTime = showTime;
+    // $scope.newLeague.rosterLimit = rosterLimit
+  }
 })
 .controller('setCharactersCtrl', function ($scope) {
 
@@ -8,4 +41,5 @@ angular.module('app.leagues.new', [])
 .controller('scoreSettingCtrl', function ($scope) {
 })
 .controller('inviteFriendsCtrl', function ($scope) {
+
 })
