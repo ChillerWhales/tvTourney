62c73053a07761547f0dc1a7932b1d97ac2af6b6
Jack Zhang <jzhang31191@gmail.com>
2015-05-30 17:57:14 -0700

    some minor changes to getEvent

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,9 @@ function setup(app, routeHandlers) {
 		.get(routeHandlers.logoutGET);
   app.route('/league/')
     .post(routeHandlers.leagueCreatePOST);
-
+	app.route('/league/:id/events')
+    .get(routeHandlers.eventGET)
+    .post(routeHandlers.eventPOST);
 	/*adding auth here protects the route from unauthenticated users
 	if user is authenticated, auth will call next and the next routehandler
 	will catch the requeset and process it, otherwise the user will receive
