c2b4c156bf73834128e30f495f1a8277f9304461
Jack Zhang <jzhang31191@gmail.com>
2015-05-30 19:33:52 -0700

    Added userHome leagues.html and userHome controller and factory

--- client/app/leagues/leagueController.js
+++ client/app/leagues/leagueController.js
@@ -3,6 +3,32 @@ angular.module('app.leagues', [
   'app.leagues.new',
   'app.leagues.draft'
 ])
-.controller('leagueController', function ($scope) { 
-
+.controller('leagueController', function ($scope, leagueHandler) { 
+  $scope.leagues = [];
+  $scope.getLeagues = function() {
+    /*uses our getLeagues handler function to GET request on server
+    for the list of leagues the user is associated with.*/
+    leagueHandler.getLeagues()
+      .then(function(data) {
+        data.each(function(league) {
+          $scope.leagues.push(league);
+        });
+      })
+  }
+  $scope.getLeagues();
+})
+//factory for handling get and post request to server for league creation activity
+.factory('leagueHandler', function($http) {
+  var getLeagues = function() {
+    return $http({
+      method: 'GET',
+      url: '/leagues'
+    }).then(function(resp) {
+      //haven't tested what the response data looks like yet subject to change
+      return resp.data;
+    });
+  };
+  return {
+    'getLeagues' : getLeagues;
+  }
 });
