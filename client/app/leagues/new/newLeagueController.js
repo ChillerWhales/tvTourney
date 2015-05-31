b66a96952630751d9738aebf214250bf8c38cdce
Monica Engel <monicaengel9@gmail.com>
2015-05-31 12:38:02 -0700

    [refactor] Fixed syntax errors on controllers

--- client/app/leagues/new/newLeagueController.js
+++ client/app/leagues/new/newLeagueController.js
@@ -1,13 +1,11 @@
 angular.module('app.leagues.new', [])
 .controller('createLeagueCtrl', function ($scope) {
   $scope.leagueName;
-});
+})
 .controller('setCharactersCtrl', function ($scope) {
-  $scope.leagueName;
-});
+
+})
 .controller('scoreSettingCtrl', function ($scope) {
-  $scope.leagueName;
-});
+})
 .controller('inviteFriendsCtrl', function ($scope) {
-  $scope.leagueName;
-});
+})
