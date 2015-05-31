33629f47ea6673c5bf8747c501afce9b30f80cc9
Jack Zhang <jzhang31191@gmail.com>
2015-05-31 10:53:11 -0700

    fixed league controller to show proper content. Move everything to list.js//html

--- client/app/leagues/list/listLeagueController.js
+++ client/app/leagues/list/listLeagueController.js
@@ -1,4 +1,30 @@
 angular.module('app.leagues.list', [])
-.controller('listLeagueController', function ($scope) { 
-
+.controller('listController', function ($scope, listHandler) { 
+  $scope.leagues = [];
+  $scope.getLeagues = function() {
+    /*uses our getLeagues handler function to GET request on server
+    for the list of leagues the user is associated with.*/
+    leagueHandler.getLeagues()
+      .then(function(data) {
+        data.each(function(league) {
+          $scope.leagues.push(league);
+        });
+      })
+  }
+  $scope.getLeagues();
+})
+//factory for handling get and post request to server for league creation activity
+.factory('listHandler', function($http) {
+  var getLeagues = function() {
+    return $http({
+      method: 'GET',
+      url: '/leagues'
+    }).then(function(resp) {
+      //haven't tested what the response data looks like yet subject to change
+      return resp.data;
+    });
+  };
+  return {
+    'getLeagues' : getLeagues
+  }
 });
