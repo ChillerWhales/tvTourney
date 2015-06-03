75025ab5eebb349f750506a9ef8812e1572e31f0
Richard Artoul <richardartoul@gmail.com>
2015-06-03 16:14:19 -0700

    [Feature] current score in user roster entries automatically get updated when an event is triggered on a character

--- server/db/models/CharacterEvent.js
+++ server/db/models/CharacterEvent.js
@@ -2,25 +2,25 @@ var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, tableConfig) {
 	tableConfig.hooks = {
+		/*this might drive you nuts, but take a look at it. Its worth it. What it does is when an event is triggered
+		on a character, it automatically triggers a cascade that causes the scoe in the user roster table to be incremented
+		by the point value of the event */
 		afterCreate: function(characterEvent, options, fn) {
-			// sequelize.league_character.find()
+			//lookup the event that is being triggered so we can get the point (score_up) value
 			sequelize.models.league_event.find({
 				where: {id: characterEvent.league_event_id}
 			}).then(function(event) {
-				// console.log("event is:", event);
-				// console.log("character event is:", characterEvent);
-				//score is in event.dataValues.score_up
-				//in characterEvent.dataValues we have id, league_id, league_character_id, league_event_id
-				console.log(characterEvent.dataValues.league_id);
-				console.log(characterEvent.dataValues.league_character_id);
-				sequelize.models.user_roster.find({
-					where: {
+				//find all the entries in the user_roster table and increment them
+				sequelize.models.user_roster.update(
+					//increment by point value of event
+					{current_score: sequelize.literal('current_score + ' + event.get('score_up'))},
+					//increment the current score for the specific character in all users (in that league) roster by the event point value
+					{where: {
 						league_id: characterEvent.dataValues.league_id,
 						league_character_id: characterEvent.dataValues.league_character_id
-					}
-				}).then(function(userRosters) {
-					console.log("Affected rows:", userRosters);
-					fn(null, characterEvent);
+					}}).then(function(affectedRows) {
+						//equivalent to done() or next()
+						fn(null, affectedRows);
 				})
 			})
 		}
