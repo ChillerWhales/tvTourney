2b0b0985cfaa16729bea05dd38784664edf7ebea
Richard Artoul <richardartoul@gmail.com>
2015-06-08 15:04:16 -0700

    [Feature] user scores in league view now update in realtime using websockets

--- server/socketSetup.js
+++ server/socketSetup.js
@@ -3,24 +3,25 @@ var sockets = require('socket.io');
 
 var leagues = {};
 
-//socket object is passed to this function and it adds all the event listeners and handlers
+//express server object is passed to this function and it attaches websockets + all the event listeners and handlers
 module.exports = function(server) {
 	//this is what actually attaches socket.io to the express server
 	io = sockets(server);
 	io.on('connection', function(socket) {
+		//when a connection is established, the client is notified
 		socket.emit('success', 'Connected successfully!');
-		console.log("New socket connection established!");
 		logger.info("New socket connection established!");
-		
+
+		/*Once the client is notified of a successful connection, it will tell the server which league it is on
+		and all users viewing the same league will be added to a socket.io "room" so that socket.io can emit to them all simultaneously*/
 		socket.on('joinLeague', function(joinRequest) {
-			console.log(joinRequest.leagueId);
-			console.log("User joined socket room for league: ", joinRequest.leagueId)
 			logger.info("User joined socket room for league: ", joinRequest.leagueId)
 			socket.join(joinRequest.leagueId);
 		});
 
+		/* When a client notifies the server that an event that has been triggered, the server passes that message along to all
+		clients that are looking at the same league which the event was triggered on */
 		socket.on('triggerEvent', function(triggeredEvent) {
-			console.log(triggeredEvent, "was emitted to league: ", triggeredEvent.leagueId);
 			logger.info(triggeredEvent, "was emitted to league: ", triggeredEvent.leagueId);
 			io.to(triggeredEvent.leagueId).emit('triggerEvent', triggeredEvent);
 		})
